d0ffacc779c3ab223e9f5eacdecf539b5ee40f9f
Edu <elprogramadorgt8@gmail.com>
2025-05-11 14:41:19 -0600

    ++ format code

--- main.c
+++ main.c
@@ -3,36 +3,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stddef.h>
 
 void *eduMalloc(size_t size);
 
-int main(){
-
+int main() {
     void *initial_break = sbrk(0);
-
     printf("Initial break: %p\n", initial_break);
 
-    void *ptr = eduMalloc(40);
+    void *ptr = eduMalloc(40);  
 
     void *final_break = sbrk(0);
-
     printf("Final break: %p\n", final_break);
 
-    printf("Final Memory %td\n", (short *)final_break - (short *)initial_break); 
+    printf("Memory used: %td bytes\n", (char *)final_break - (char *)initial_break);
 
+    return 0;
 }
 
+void *eduMalloc(size_t size) {
+    void *ptr = sbrk(size);  
 
-void *eduMalloc(size_t size){
-
-    void *ptr = sbrk(40);
-
-    if(ptr == (void *)-1){
-        return NULL;
+    if (ptr == (void *)-1) {
+        return NULL; 
     }
 
     return ptr;
-
-
-
 }
